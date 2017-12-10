c5717d25df5b469867fad61b465ae6645778ec9e
unknown <tony@gmail.com>
2017-12-10 16:43:42 +0800

    修改程序图标和名称。

--- app/src/main/java/com/coolweather/android/service/AutoUpdateService.java
+++ app/src/main/java/com/coolweather/android/service/AutoUpdateService.java
@@ -51,7 +51,7 @@ public class AutoUpdateService extends Service {
             String weatherId = weather.basic.weatherId;
 
             String weatherUrl = "http://guolin.tech/api/weather?cityid=" +
-                    weatherId + "&key=bc0418b57b2d4918819d3974ac1285d9";
+                    weatherId + "&key=f36303a624d34f1096fc9f9f0f1ee414";
             HttpUtil.sendOkHttpRequest(weatherUrl, new Callback() {
                 @Override
                 public void onFailure(Call call, IOException e) {
